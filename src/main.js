02cdf439036843be0dc9d456ceef880969c2abbd
jesuskostov <jesuskostov@gmail.com>
2022-03-15 18:20:23 +0200

    123

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueDragDrop from 'vue-drag-drop';
 
 Vue.config.productionTip = false
+Vue.use(VueDragDrop);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <div class="letters-wrapper">
-      <p v-for="(letter, i) in letters" :key="i">{{ letter }}</p>
+      <drop @dragover="currentIndex = i" @drop="handleDrop" class="drop" v-for="(letter, i) in letters" :key="i">{{ letter }}</drop>
+    </div>
+    <div class="letters-wrapper">      
+      <drag class="drag" v-for="(letter, i) in missingLetters" :key="i" :transfer-data="letter" >{{ letter }}</drag>
     </div>
     <input type="text" v-model="text" placeholder="Insert one word">
     <div>
@@ -24,11 +27,26 @@ export default {
       currentWord: '',
       word: 'RANDOM',
       letters: [],
-      text: ''
+      text: '',
+      missingLetters: [],
+      currentIndex: null
     }
   },
   methods: {
+    handleDrop(data) {
+      let draggedLetter = JSON.stringify(data).split('"')[1]
+      let word = this.word.split('')
+      if (word.indexOf(draggedLetter) === this.currentIndex) {
+        this.letters[this.currentIndex] = draggedLetter
+        let memory = this.letters
+        this.letters = []
+        this.letters = memory
+        let i = this.missingLetters.indexOf(draggedLetter)
+        this.missingLetters.splice(i, 1);
+      }
+    },
     makeGame() {
+      this.missingLetters = []
       let letters = this.word.split('')
       
       let max = letters.length - 1
@@ -47,7 +65,8 @@ export default {
         } 
       })
       setTimeout(() => {
-        letterForRemove.map(i => {          
+        letterForRemove.map(i => {
+          this.missingLetters.push(letters[i])
           letters[i] = '?'
           this.letters = letters
         })
@@ -71,7 +90,8 @@ export default {
   display: flex;
   justify-content: center;
   gap: 30px;
-  p {
+  .drop, .drag {
+    margin-bottom: 50px;
     background-color: #000;
     border-radius: 12px;
     min-width: 100px;
@@ -79,6 +99,9 @@ export default {
     font-size: 80px;
     color: #fff;
     font-weight: bold;
+  }
+  .drag {
+    cursor: move;
   }
 }
 
